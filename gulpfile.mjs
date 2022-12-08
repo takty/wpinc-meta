1c9d149d7c4d8abb64a505758f5e69c82147628f
Takuto Yanagida <yanagida@stxst.com>
2022-12-08 15:50:48 +0900

    Update gulp process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,11 +2,8 @@
  * Gulp file
  *
  * @author Takuto Yanagida
- * @version 2022-03-23
+ * @version 2022-12-08
  */
-
-/* eslint-disable no-undef */
-'use strict';
 
 const SRC_JS_RAW  = ['src/**/*.js', '!src/**/*.min.js'];
 const SRC_JS_MIN  = ['src/**/*.min.js'];
@@ -14,21 +11,14 @@ const SRC_SASS    = ['src/**/*.scss'];
 const SRC_CSS_RAW = ['src/**/*.css', '!src/**/*.min.css'];
 const SRC_CSS_MIN = ['src/**/*.min.css'];
 const SRC_PHP     = ['src/**/*.php'];
-const SRC_PO      = ['src/languages/**/*.po'];
-const SRC_JSON    = ['src/languages/**/*.json'];
 const DEST        = './dist';
 
-const gulp = require('gulp');
+import gulp from 'gulp';
 
-const { makeCopyTask }   = require('./task-copy');
-const { makeCssTask }    = require('./task-css');
-const { makeJsTask }     = require('./task-js');
-const { makeLocaleTask } = require('./task-locale');
-const { makeSassTask }   = require('./task-sass');
-
-
-// -----------------------------------------------------------------------------
-
+import { makeJsTask } from './gulp/task-js.mjs';
+import { makeCssTask } from './gulp/task-css.mjs';
+import { makeSassTask } from './gulp/task-sass.mjs';
+import { makeCopyTask } from './gulp/task-copy.mjs';
 
 const js_raw  = makeJsTask(SRC_JS_RAW, DEST, 'src');
 const js_min  = makeCopyTask(SRC_JS_MIN, DEST);
@@ -38,8 +28,6 @@ const css_min = makeCopyTask(SRC_CSS_MIN, DEST);
 const css     = gulp.parallel(css_raw, css_min);
 const sass    = makeSassTask(SRC_SASS, DEST);
 const php     = makeCopyTask(SRC_PHP, DEST);
-const po      = makeLocaleTask(SRC_PO, DEST, 'src');
-const json    = makeCopyTask(SRC_JSON, DEST, 'src');
 
 const watch = done => {
 	gulp.watch(SRC_JS_RAW, gulp.series(js_raw));
@@ -48,10 +36,8 @@ const watch = done => {
 	gulp.watch(SRC_CSS_RAW, gulp.series(css_raw));
 	gulp.watch(SRC_CSS_MIN, gulp.series(css_min));
 	gulp.watch(SRC_PHP, gulp.series(php));
-	gulp.watch(SRC_PO, po);
-	gulp.watch(SRC_JSON, json);
 	done();
 };
 
-exports.build   = gulp.parallel(js, sass, css, php, po, json);
-exports.default = gulp.series(exports.build , watch);
+export const build = gulp.parallel(js, sass, css, php);
+export default gulp.series(build , watch);
