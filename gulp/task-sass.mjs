62b2a799f6ccac85480897af1ad34abb2cab353d
Takuto Yanagida <yanagida@stxst.com>
2023-06-21 11:16:03 +0900

    Update packages

--- gulp/task-sass.mjs
+++ gulp/task-sass.mjs
@@ -2,7 +2,7 @@
  * Function for gulp (SASS)
  *
  * @author Takuto Yanagida
- * @version 2023-06-02
+ * @version 2023-06-21
  */
 
 const SASS_OUTPUT_STYLE = 'compressed';  // 'expanded' or 'compressed'
@@ -13,7 +13,7 @@ import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
 import changed from 'gulp-changed';
 
-import dartSass from 'sass';
+import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
 
