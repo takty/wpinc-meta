ef450c6fe4b22a41f10eff772c1c9eaf00c72089
Takuto Yanagida <yanagida@stxst.com>
2023-12-28 00:00:00 +0900

    Update packages

--- gulp/task-css.mjs
+++ gulp/task-css.mjs
@@ -2,7 +2,7 @@
  * Function for gulp (CSS)
  *
  * @author Takuto Yanagida
- * @version 2022-09-16
+ * @version 2023-11-08
  */
 
 import gulp from 'gulp';
@@ -10,7 +10,7 @@ import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
 import cleanCss from 'gulp-clean-css';
 import rename from 'gulp-rename';
-import changed from 'gulp-changed';
+import changed, { compareContents } from 'gulp-changed';
 
 export function makeCssTask(src, dest = './dist', base = null) {
 	const cssTask = () => gulp.src(src, { base: base, sourcemaps: true })
@@ -18,7 +18,7 @@ export function makeCssTask(src, dest = './dist', base = null) {
 		.pipe(autoprefixer({ remove: false }))
 		.pipe(cleanCss())
 		.pipe(rename({ extname: '.min.css' }))
-		.pipe(changed(dest, { hasChanged: changed.compareContents }))
+		.pipe(changed(dest, { hasChanged: compareContents }))
 		.pipe(gulp.dest(dest, { sourcemaps: '.' }));
 	return cssTask;
 }
