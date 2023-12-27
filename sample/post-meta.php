2f9a050acf5b09f4261113515e777dd0d31aad30
Takuto Yanagida <yanagida@stxst.com>
2023-12-27 23:59:53 +0900

    Add samples

--- sample/post-meta.php
+++ sample/post-meta.php
@@ -4,12 +4,13 @@
  *
  * @package Wpinc Meta
  * @author Takuto Yanagida
- * @version 2023-10-13
+ * @version 2023-12-27
  */
 
 namespace wpinc\meta\sample;
 
 require_once __DIR__ . '/../src/field.php';
+require_once __DIR__ . '/../src/field-post.php';
 require_once __DIR__ . '/../src/key-suffix.php';
 require_once __DIR__ . '/../src/picker.php';
 require_once __DIR__ . '/../src/utility.php';
@@ -17,23 +18,25 @@ require_once __DIR__ . '/../src/utility.php';
 /**
  * Adds a metabox of post meta sample.
  *
- * @param string $pt  Post type.
- * @param string $tax Taxonomy.
+ * @param string     $pt  Post type.
+ * @param \WP_Post[] $ps  Posts.
+ * @param string     $tax Taxonomy.
  */
-function add_post_meta_sample( string $pt, string $tax ): void {
+function add_post_meta_sample( string $pt, array $ps, string $tax ): void {
 	add_action(
 		'admin_menu',
-		function () use ( $pt, $tax ) {
+		function () use ( $pt, $ps, $tax ) {
 			add_meta_box(
 				'mb_post_meta_sample',
 				'Post Meta Sample',
-				function ( \WP_Post $post ) use ( $tax ): void {
+				function ( \WP_Post $post ) use ( $ps, $tax ): void {
 					$post_id = $post->ID;
 					\wpinc\meta\add_input_to_post( $post_id, '_meta_input', 'Input' );
 					\wpinc\meta\add_textarea_to_post( $post_id, '_meta_textarea', 'Textarea' );
 					\wpinc\meta\add_separator_to_post();
 					\wpinc\meta\add_rich_editor_to_post( $post_id, '_meta_rich_editor', 'Rich Editor' );
 					\wpinc\meta\add_checkbox_to_post( $post_id, '_meta_checkbox', 'Checkbox', 'Checkbox title' );
+					\wpinc\meta\add_post_select_to_post( $post_id, '_meta_post_select', 'Post Select', $ps );
 					\wpinc\meta\add_term_select_to_post( $post_id, '_meta_term_select', 'Term Select', $tax );
 					\wpinc\meta\add_related_term_select_to_post( $post_id, '_meta_related_term_select', 'Related Term Select', $tax );
 					\wpinc\meta\add_separator_to_post();
@@ -60,6 +63,7 @@ function add_post_meta_sample( string $pt, string $tax ): void {
 			\wpinc\meta\set_post_meta( $post_id, '_meta_textarea' );
 			\wpinc\meta\set_post_meta( $post_id, '_meta_rich_editor' );
 			\wpinc\meta\set_post_meta( $post_id, '_meta_checkbox' );
+			\wpinc\meta\set_post_meta( $post_id, '_meta_post_select' );
 			\wpinc\meta\set_post_meta( $post_id, '_meta_term_select' );
 			\wpinc\meta\set_post_meta( $post_id, '_meta_related_term_select' );
 			\wpinc\meta\set_post_meta_suffix( $post_id, '_meta_input_suffix', array( 'ja', 'en' ) );
--- sample/term-meta.php
+++ sample/term-meta.php
@@ -4,12 +4,13 @@
  *
  * @package Wpinc Meta
  * @author Takuto Yanagida
- * @version 2023-10-13
+ * @version 2023-12-27
  */
 
 namespace wpinc\meta\sample;
 
 require_once __DIR__ . '/../src/field.php';
+require_once __DIR__ . '/../src/field-term.php';
 require_once __DIR__ . '/../src/key-suffix.php';
 require_once __DIR__ . '/../src/picker.php';
 require_once __DIR__ . '/../src/utility.php';
@@ -17,18 +18,20 @@ require_once __DIR__ . '/../src/utility.php';
 /**
  * Adds a metabox of term meta sample.
  *
- * @param string $tax Taxonomy.
+ * @param string     $tax Taxonomy.
+ * @param \WP_Post[] $ps  Posts.
  */
-function add_term_meta_sample( string $tax ): void {
+function add_term_meta_sample( string $tax, array $ps ): void {
 	add_action(
 		$tax . '_edit_form_fields',
-		function ( \WP_Term $term ): void {
+		function ( \WP_Term $term ) use ( $ps ): void {
 			$term_id = $term->term_id;
 			\wpinc\meta\add_input_to_term( $term_id, '_meta_input', 'Input' );
 			\wpinc\meta\add_textarea_to_term( $term_id, '_meta_textarea', 'Textarea' );
 			\wpinc\meta\add_separator_to_term();
 			\wpinc\meta\add_rich_editor_to_term( $term_id, '_meta_rich_editor', 'Rich Editor' );
 			\wpinc\meta\add_checkbox_to_term( $term_id, '_meta_checkbox', 'Checkbox', 'Checkbox title' );
+			\wpinc\meta\add_post_select_to_term( $term_id, '_meta_post_select', 'Post Select', $ps );
 			\wpinc\meta\add_separator_to_term();
 			\wpinc\meta\add_input_suffix_to_term( $term_id, '_meta_input_suffix', array( 'ja', 'en' ), 'Input Suffix' );
 			\wpinc\meta\add_textarea_suffix_to_term( $term_id, '_meta_textarea_suffix', array( 'ja', 'en' ), 'Input Suffix' );
@@ -51,6 +54,7 @@ function add_term_meta_sample( string $tax ): void {
 			\wpinc\meta\set_term_meta( $term_id, '_meta_textarea' );
 			\wpinc\meta\set_term_meta( $term_id, '_meta_rich_editor' );
 			\wpinc\meta\set_term_meta( $term_id, '_meta_checkbox' );
+			\wpinc\meta\set_term_meta( $term_id, '_meta_post_select' );
 			\wpinc\meta\set_term_meta_suffix( $term_id, '_meta_input_suffix', array( 'ja', 'en' ) );
 			\wpinc\meta\set_term_meta_suffix( $term_id, '_meta_textarea_suffix', array( 'ja', 'en' ) );
 			\wpinc\meta\set_term_meta( $term_id, '_meta_media_picker' );
